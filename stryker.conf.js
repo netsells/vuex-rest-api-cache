0471006ca7d15ea5ffe167c8ffe3be778748c1bf
Sam Boylett <sam.boylett@netsells.co.uk>
2018-12-14 16:20:44 +0000

    trigger build

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,13 +1,13 @@
 module.exports = function(config) {
   config.set({
-    mutator: "javascript",
-    packageManager: "yarn",
-    reporters: ["html", "progress", "dashboard"],
-    testRunner: "jest",
-    transpilers: ["babel"],
-    coverageAnalysis: "off",
-    mutate: ["src/**/*.js"],
-    babelrcFile: ".babelrc",
+    mutator: 'javascript',
+    packageManager: 'yarn',
+    reporters: ['html', 'progress', 'dashboard'],
+    testRunner: 'jest',
+    transpilers: ['babel'],
+    coverageAnalysis: 'off',
+    mutate: ['src/**/*.js'],
+    babelrcFile: '.babelrc',
     thresholds: {
         break: 100,
         high: 100,
